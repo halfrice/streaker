5532380981ebf7a77450869c418b9a90a1cd0644
halfrice <halfrice@protonmail.com>
2022-04-25 23:41:57 -0700

    Use react hooks for home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback, useState } from "react"
 import styled from "styled-components"
 import { devices } from "@styles"
 
@@ -14,51 +14,102 @@ const StyledHome = styled.div`
 const Title = styled.h1`
   color: var(--blue);
   ${devices.tablet`color: var(--purple)`};
+  margin-bottom: 1rem;
 `
 
-const MessageTextArea = styled.textarea`
+const StyledMessageRow = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
   width: 100%;
-  max-width: 500px;
-  min-height: 100px;
-`
+  max-height: 3rem;
+  margin: 0 auto;
 
-const SendButton = styled.button`
-  margin-top: 20px;
+  input {
+    width: 100%;
+    max-width: 600px;
+    height: 3rem;
+    background-color: transparent;
+    font-size: 24px;
+    color: var(--eigengrau);
+    resize: none;
+    border: 1px solid #a4bdde;
+    border-radius: 3px;
+  }
+
+  button {
+    height: 3rem;
+    margin: 0 0.5rem;
+    color: var(--eigengrau);
+    background-color: transparent;
+    text-decoration: none;
+    cursor: pointer;
+    border: 1px solid #a4bdde;
+    border-radius: 3px;
+
+    &:focus,
+    &:active {
+      border: 2px solid blue;
+    }
+
+    &:hover {
+      background-color: var(--light-green);
+    }
+
+    span {
+      margin: 0.5rem;
+      padding: 0.5rem 0;
+    }
+  }
 `
 
 const CurrentMessage = styled.div`
-  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: var(--blue);
+  margin-top: 1rem;
+
+  span {
+    margin-top: 0.5rem;
+    font-size: 24px;
+    color: var(--eigengrau);
+  }
 `
 
-class Home extends React.Component {
-  state = {
-    textAreaValue: "",
-    message: "",
+const Home = () => {
+  const [textAreaValue, setTextAreaValue] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleTextAreaChange = (event) => {
+    setTextAreaValue(event.target.value)
   }
 
-  handleTextAreaChange = (event) => {
-    this.setState({ textAreaValue: event.target.value })
+  const handleSendMessage = () => {
+    setMessage(textAreaValue)
   }
 
-  sendMessage = () => {
-    this.setState({ message: this.state.textAreaValue })
-  }
-
-  render() {
-    return (
-      <StyledHome>
-        <Title>Streaker</Title>
-        <MessageTextArea
+  return (
+    <StyledHome>
+      <Title>Streaker</Title>
+      <StyledMessageRow>
+        <input
           type="text"
-          onChange={this.handleTextAreaChange}
-          value={this.state.textAreaValue}
+          onChange={handleTextAreaChange}
+          value={textAreaValue}
           placeholder="Enter message"
-        ></MessageTextArea>
-        <SendButton onClick={this.sendMessage}>Enter</SendButton>
-        <CurrentMessage>Sent Message: {this.state.message}</CurrentMessage>
-      </StyledHome>
-    )
-  }
+          rows="1"
+        ></input>
+        <button onClick={handleSendMessage}>
+          <span>Enter</span>
+        </button>
+      </StyledMessageRow>
+      <CurrentMessage>
+        Sent Message:<span>{message}</span>
+      </CurrentMessage>
+    </StyledHome>
+  )
 }
 
 export default Home
